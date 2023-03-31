ccf9f04d3b52d62c2a2f235856403eb23f6e9f14
Aglomation <113240095+Muminwilmer@users.noreply.github.com>
2023-03-31 11:48:41 +0200

    Delete codestuff.js

--- codestuff.js
+++
@@ -1,1 +0,0 @@
-javascript:(function(){var script=document.createElement('script');script.src='https://raw.githubusercontent.com/Muminwilmer/thingy/main/codeStuff.js';document.body.appendChild(script);})();
