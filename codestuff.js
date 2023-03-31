023a2d72144e59709f067e75079417e1f068fc5b
Aglomation <113240095+Muminwilmer@users.noreply.github.com>
2023-03-31 11:48:14 +0200

    Create codestuff.js

---
+++ codestuff.js
@@ -0,0 +1,1 @@
+javascript:(function(){var script=document.createElement('script');script.src='https://raw.githubusercontent.com/Muminwilmer/thingy/main/codeStuff.js';document.body.appendChild(script);})();
